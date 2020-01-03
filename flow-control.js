f3769503502413369a1fa268946e91f355588f77
Basil Kimathi <basilkimathi@gmail.com>
2020-01-03 02:21:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,18 +2,33 @@ function basicTeenager(age) {
 
 }
 
-function teenager(age) {
-
+function teenager(13-19) {
+"You are a teenager"
+} else {
+  "You are not a teenager"
 }
 
-function ageChecker(age) {
-
+function ageChecker(13-19) {
+"You are a teenager!"
 }
-
+else if (1-12){
+  "You are a kid"
+}
+else (19-80) {
+  "You are a grownup"
+}
 function ternaryTeenager(age) {
-
+if (age ==='13-19'){
+return "You are a teenager"
+}
+return "You are not a teenager"
 }
 
 function switchAge(age) {
-
+switch(age){
+case "13-19":
+return "You are a teenager"
+default:
+return "You have an age"
+}
 }
